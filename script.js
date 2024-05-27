58cf1acfdf73479908d9a485ebf4e7d405ba921e
Federico Petrocchi <petrocchi.federico@gmail.com>
2024-05-27 14:57:04 +0200

    generazione cards tramite js e relative funzioni collegate

--- script.js
+++ script.js
@@ -21,6 +21,66 @@ window.addEventListener ('scroll', () => {
 });
 
 
+let games = [
+    {name : 'Assassins Creed Shadows', price : '60€', genre : 'Action RPG', photo : './media/AC.png'},
+    {name : 'Skull and Bones', price : '40€', genre : 'Action-Adventure', photo : './media/skull-and-bones.png'},
+    {name : 'XDefiant', price : 'Free', genre : 'FPS', photo : './media/XDefiant.png'},
+    {name : 'Star Wars', price : '60€', genre : 'Action-Adventure', photo : './media/starwars.png'},
+    {name : 'Assassins Creed Mirage', price : '45€', genre : 'Action RPG', photo : './media/acm.png'},
+    {name : 'The Crew Motorfest', price : '35€', genre : 'Racing', photo : './media/tcm.png'},
+];
+
+let row = document.querySelector('#cardWrapper');
+
+function createCards(){
+games.forEach((game) => {
+    let divCol = document.createElement('div');
+    divCol.classList.add('col-12', 'col-md-2');
+    divCol.innerHTML = `
+    <div class="card border-0 position-relative" style="width: 18rem;">
+                    <img src="${game.photo}" class="card-img-top" alt="...">
+                    <i class="bi bi-cart"></i>
+                    <div class="card-body bg-black ">
+                      <h5 class="card-title displayFont text-light">${game.name}</h5>
+                      <p class="textFont text-light">${game.genre} </p>
+                      <p class="textFont text-light number1 mt-2">Più di 0 utenti </p>
+                    </div>
+                        <div class="consoles mt-2">
+                            <img src="./media/svg_.svg" alt="" class="consoleSize">
+                            <img src="./media/svg_ (1).svg" alt="" class="consoleSize">
+                            <img src="./media/svg_ (2).svg" alt="" class="consoleSize">
+                            <img src="./media/svg_ (3).svg" alt="" class="consoleSize">
+                        </div>
+                  </div>
+    `
+    row.appendChild(divCol);
+}) 
+
+// funzione icona metti nel carrello (lezione like cuoricini)
+let baskets = document.querySelectorAll('.bi-cart');
+let cardsImg = document.querySelectorAll('.card-img-top');
+
+
+    baskets.forEach((basket) => {
+        basket.addEventListener('click', () => {
+            basket.classList.toggle('bi-cart-check-fill');
+            basket.classList.toggle('bi-cart');
+        })
+    })
+    
+// funzione add to cart con doppio click su immagine
+cardsImg.forEach((cardImg, i) => {
+    cardImg.addEventListener('dblclick', () => {
+        // uso i perchè così l'evento carrello si attiverà solo sull'elemento corrispondente. Senza, si attiverebbero i carrelli di tutte le immagini
+        baskets[i].classList.toggle('bi-cart-check-fill');
+        baskets[i].classList.toggle('bi-cart');
+    })    
+})
+
+}
+
+createCards();
+
 
 // fai partire contatori
 // la generalizzo così che posso settare valori diversi per i contatori di ogni card
@@ -66,28 +126,11 @@ observer.observe(numCard1);
 
 
 
-// funzione icona metti nel carrello
-let baskets = document.querySelectorAll('.bi-cart');
-let cardsImg = document.querySelectorAll('.card-img-top');
 
-
-    baskets.forEach((basket) => {
-        basket.addEventListener('click', () => {
-            basket.classList.toggle('bi-cart-check-fill');
-            basket.classList.toggle('bi-cart');
-        })
         
         
 
-// funzione add to cart con doppio click su immagine
-        cardsImg.forEach((cardImg, i) => {
-            cardImg.addEventListener('dblclick', () => {
-                
-                basket.classList.toggle('bi-cart-check-fill');
-                basket.classList.toggle('bi-cart');
-            })    
-        })
-    })
+
 
 
 
